6e7847214830f6090afddea95704fcdfdbe467dd
Fatih Bakir <mfatihbakir@gmail.com>
2019-01-29 19:42:47 -0800

    codegen for safe union

--- compiler/Moka/Codegen/CppGen.hs
+++ compiler/Moka/Codegen/CppGen.hs
@@ -28,3 +28,17 @@ generate_members lst = foldl (++) "" (map add_newline (map generate_member lst))
 generate_struct :: StructDef -> String
 generate_struct (Structure name mems) =
     "struct " ++ (generate_name name) ++ "{\n" ++ (generate_members mems) ++ "}"
+
+make_names :: Int -> [String]
+make_names 0 = []
+make_names x = (show (length rest)):rest where rest = make_names (x - 1)
+
+generate_union :: UnionDef -> String
+generate_union (UnionT name elems) =
+    "struct " ++ (generate_name name) ++ "{\n" ++
+        "int type_;" ++
+        "union {" ++
+            generate_members [(RawMem tp (Identifier nm)) | tp <- elems
+                                                          , nm <- (make_names (length elems))] ++
+        "}" ++
+    "}"
